c3ee51a980793c3f50437131ce8e57cd0f0b888f
Wiktor Jóźwik <w.jozwik@stud.elka.pw.edu.pl>
2020-12-28 18:32:55 +0100

    Specified Post/Get/Delete requests

--- src/quotes/quotes.controller.ts
+++ src/quotes/quotes.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 @Controller('quotes')
-export class QuotesController {}
+export class QuotesController {
+  // constructor
+  @Get()
+  showAllQuotes() {}
+
+  @Get(':id')
+  readSingleQuote(@Param('id') id: string) {}
+
+  @Get(':name')
+  readStockQuotes(@Param(':name') name: string) {}
+
+  @Post()
+  createQuote() {}
+
+  @Delete(':id')
+  deleteQuote(@Param('id') id: string) {}
+}
